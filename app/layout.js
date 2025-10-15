236ee190c2d3f235ea08943e1717e7809bce71e3
rahalhabib <hello@habibrahal.com>
2025-10-15 21:24:56 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 export const metadata = { title: "Plushy Generator" };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body style={{ margin: 0, fontFamily: "ui-sans-serif" }}>{children}</body>
